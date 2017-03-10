11a9bf1a9c5f72ca1685f5ab471dac5fc4f90b78
RileyWium <wium@unbc.ca>
2017-03-10 15:34:09 -0800

    New Level Creation System
    This time I branched off of Points and Info :<

--- www/js/Levels.js
+++ www/js/Levels.js
@@ -6,31 +6,31 @@ var levels = {
         winCondition: function(thisRef) {
 
         },
-        onStart: function(thisRef) {
-            thisRef.addGoal(200,150);
-        },
+       // onStart: function(thisRef) {
+        //    thisRef.addGoal(200,150);
+       // },
         onUpdate: function(thisRef) {
 
         },
         attribute1: {name:"gravity",max:1000,min:0,init:500,jump:100},
         attribute2: {name:"velocity",max:500,min:-500,init:0,jump:50},
         attribute3: {name:"elasticity",max:10,min:0,init:0,jump:1},
-        characterInfo: {x:200,y:200,name:"avatar"}
+        //characterInfo: {x:200,y:200,name:"avatar"}
     },
     level2: {
         winCondition: function(thisRef) {
 
         },
-        onStart: function(thisRef) {
+       // onStart: function(thisRef) {
 
-        },
+       // },
         onUpdate: function(thisRef) {
 
         },
         attribute1: {name:"gravity",max:10,min:0,init:0,jump:1},
         attribute2: {name:"velocity",max:10,min:0,init:0,jump:1},
         attribute3: {name:"elasticity",max:10,min:0,init:0,jump:1},
-        characterInfo: {x:200,y:200,name:"avatar"}
+       // characterInfo: {x:200,y:200,name:"avatar"}
     }
 
 };
