c9f618e2d3cf64544c153abf8c07ba41b32cc58b
Davis Goulet <davis.goulet@gmail.com>
2017-04-07 20:40:53 -0700

    Fixed Looping System

--- www/js/LoopQueue.js
+++ www/js/LoopQueue.js
@@ -1,64 +1,66 @@
 //This file holds all the logic behind the loop queue.
 
-
-
-
-
 //Constructor for a new loopQueue object.
 function LoopQueue(queueSize)
 {
     this.maxQueueSize = queueSize;
     this.table = document.getElementById("queueTable");
-    //this.row = this.table.insertRow(0);
+    this.row = this.table.insertRow(0);
     this.currentQueueSize = 0;
+    this.currentElement = 0;
     this.LoopArray = [];
     this.BoolArray = []; 
-    this.BeginArray = false; 
-   // this.Attributes = new Attributes(); 
+    this.AddingToArray = false;
+    this.firstIteration = true;
+    this.points = 0;
 }
 
 //Appends an element to the queue.
-LoopQueue.prototype.addToQueue = function(Attribute,Direction)
+LoopQueue.prototype.addToQueue = function(attribute,direction)
 {
-    //Only add an element if there is room in the queue.
-    if(this.currentQueueSize < this.maxQueueSize)
+    //Only add an element if there is room in the queue and the user has signaled to add it to the array.
+    if(this.currentQueueSize < this.maxQueueSize && this.AddingToArray)
     {
+        this.LoopArray.push(attribute);
+        this.BoolArray.push(direction);
         //Adds a new element to the end of the queue, then set its content.
         var x = this.row.insertCell(-1);
-        this.LoopArray.push(Attribute); 
-        this.BoolArray.push(Direction); 
-        console.log(Attribute +" Pushed to array, size is now: "+this.LoopArray.length); 
-        //TODO this should be changed to the sprite iteslf, this can be based on the attribute number that gets passed. 
-        var l; 
-        //hard coded for now. switch out later
-        switch(Attribute)
-                {
-            case 1:
-                    if(Direction == false)
-                {
-                l = "spr_gravityDecrease.png";
-                }
+        var imageToAdd;
+        switch(attribute)
+        {
+            case "gravity":
+                if(direction == false)
+                    imageToAdd = "spr_gravityDecrease.png";
                 else
-                l = "spr_gravityIncrease.png"; 
-                
-            case 2:
-                 if(Direction == false)
-                {
-                l = "spr_velocityLeft.png";
-                }
+                    imageToAdd = "spr_gravityIncrease.png";
+                break;
+            case "velocity":
+                if(direction == false)
+                    imageToAdd = "spr_velocityLeft.png";
                 else
-                l = "spr_velocityRight.png"; 
-            case 3:
-                 if(Direction == false)
-                {
-                l = "spr_springDecrease.png";
-                }
+                    imageToAdd = "spr_velocityRight.png";
+                break;
+            case "bounce":
+                if(direction == false)
+                    imageToAdd = "spr_springDecrease.png";
                 else
-                l = "spr_springIncrease.png"; 
+                    imageToAdd = "spr_springIncrease.png";
+                break;
+            case "friction":
+                if(direction == false)
+                    imageToAdd = "spr_frictionDown.png";
+                else
+                    imageToAdd = "spr_frictionUp.png";
+                break;
+            case "acceleration":
+                if(direction == false)
+                    imageToAdd = "spr_accelerationLeft.png";
+                else
+                    imageToAdd = "spr_accelerationRight.png";
+                break;
+
         }
-        
-        x.innerHTML=" <img src=\"assets/images/Buttons/"+l+"\" >";
-        //add switch for attribute pics
+        x.innerHTML="<img src=\"assets/images/Buttons/"+imageToAdd+"\">";
         this.currentQueueSize++;
     }
 
@@ -68,51 +70,61 @@ LoopQueue.prototype.addToQueue = function(Attribute,Direction)
 LoopQueue.prototype.resetQueue = function()
 {
     //Delete the current row, then create a new o   ne that is empty.  this is causing problems
-   // this.table.deleteRow(0);
-    //this.row = this.table.insertRow(0);
+    this.table.deleteRow(0);
+    this.row = this.table.insertRow(0);
     this.currentQueueSize = 0;
     //clear array
     this.LoopArray = []; 
-    this.BeginArray = false; 
-    console.log("BeginArray is equal to " + this.BeginArray); 
-};
-//begins the looping process, basically turns it on. this will set a bool that will now, when buttons are pressed send them into the Looparray to be stored and read from. 
-LoopQueue.prototype.startToQueue = function()
-{
-    this.BeginArray = true; 
-    console.log("BeginArray is equal to " + this.BeginArray); 
-}; 
-//get beginarray
-LoopQueue.prototype.GetBeginArray = function()
-{
-
-    return this.BeginArray; 
-
+    this.AddingToArray = false;
+    this.firstIteration = true;
 };
 
-/*
-//Start itterating through the array and sending the information to move the char
-//this will go through the loop, and pass the information into the updatefrombuttons method
-//if the game is paused during this period, the array will clear. 
-//when it finishes the array, it starts again. 
-LoopQueue.prototype.BeginLoop = function()
+//Add the button press to the quote instead of triggering it right away.
+LoopQueue.prototype.startAddingToArray = function()
 {
-    this.BeginArray = false; 
-    console.log("the boolean is: "+this.BeginArray); 
-    console.log("Length of array: "+this.LoopArray.length); 
-    //this is a forever loop im not smart
-    //while the game is not paused loop
-    var a =0; 
-//if not paused go
-   
-    for(var i = 0; i<this.LoopArray.length; i++)
-        {
-            attributes.updateAttributeAmountFromButton(this.LoopArray[i], this.BoolArray[i]); 
-            console.log("using"+this.LoopArray[i]+ " with " + this.BoolArray[i]); 
-            //go back to the start
-        }
-        
-    
+    this.AddingToArray = true;
 };
-*/
 
+//The user has finished adding elements to the queue.
+LoopQueue.prototype.stopAddingToArray = function()
+{
+    this.AddingToArray = false;
+};
+
+LoopQueue.prototype.getNextElement = function()
+{
+    //Return null if there are no elements in the array.
+    if(this.LoopArray.length == 0)
+        return null;
+    var element = [this.LoopArray[this.currentElement],this.BoolArray[this.currentElement]];
+
+    if(!this.firstIteration)
+    {
+        if(this.currentElement == 0)
+            this.points++;
+    }
+    else
+    {
+        this.points++;
+    }
+
+    //Increment the currentElement counter. If it has reached the end of the array, loop back to beginning.
+    this.currentElement++;
+    if(this.currentElement >= this.LoopArray.length)
+    {
+        this.firstIteration = false;
+        this.currentElement = 0;
+    }
+    return element;
+};
+
+//Returns the amount of points;
+LoopQueue.prototype.getPoints = function()
+{
+    return this.points;
+};
+
+LoopQueue.prototype.incrementPoints = function()
+{
+    this.points++;
+};
